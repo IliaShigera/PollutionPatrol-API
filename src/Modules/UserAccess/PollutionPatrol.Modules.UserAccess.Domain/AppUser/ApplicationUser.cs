9b22e3511d726880a97e150ea605a5c09e7d040d
Ilya <ShigeraIlia@gmail.com>
2023-05-03 15:54:18 -0400

    UserAccess: add ApplicationUser domain model

---
+++ src/Modules/UserAccess/PollutionPatrol.Modules.UserAccess.Domain/AppUser/ApplicationUser.cs
@@ -0,0 +1,49 @@
+namespace PollutionPatrol.Modules.UserAccess.Domain.AppUser;
+
+public sealed class ApplicationUser : Entity
+{
+    public ApplicationUser(
+        string firstName,
+        string emailAddress,
+        string passwordHash,
+        string salt,
+        List<UserRole> roles)
+    {
+        FirstName = firstName;
+        EmailAddress = emailAddress;
+        PasswordHash = passwordHash;
+        Salt = salt;
+        Roles = roles;
+    }
+
+    private ApplicationUser()
+    {
+        // EF only
+    }
+
+    public string FirstName { get; private set; }
+    public string EmailAddress { get; private set; }
+    public string PasswordHash { get; private set; }
+    public string Salt { get; private set; }
+    public List<UserRole> Roles { get; private set; }
+
+    internal static ApplicationUser CreateFromRegistration(UserRegistration registration)
+    {
+        return new ApplicationUser(
+            registration.FirstName,
+            registration.EmailAddress,
+            registration.PasswordHash,
+            registration.Salt,
+            roles: new List<UserRole> { UserRole.User });
+    }
+
+    public static ApplicationUser CreateAdmin(string firstName, string emailAddress, string passwordHash, string salt)
+    {
+        return new ApplicationUser(
+            firstName,
+            emailAddress,
+            passwordHash,
+            salt,
+            roles: new List<UserRole> { UserRole.Admin });
+    }
+}
--- src/Modules/UserAccess/PollutionPatrol.Modules.UserAccess.Domain/GlobalUsings.cs
+++ src/Modules/UserAccess/PollutionPatrol.Modules.UserAccess.Domain/GlobalUsings.cs
@@ -2,5 +2,6 @@
 
 global using PollutionPatrol.BuildingBlocks.Domain.Interfaces;
 global using PollutionPatrol.BuildingBlocks.Domain.Models;
+global using PollutionPatrol.Modules.UserAccess.Domain.Registration;
 global using PollutionPatrol.Modules.UserAccess.Domain.Registration.Events;
 global using PollutionPatrol.Modules.UserAccess.Domain.Registration.Rules;
