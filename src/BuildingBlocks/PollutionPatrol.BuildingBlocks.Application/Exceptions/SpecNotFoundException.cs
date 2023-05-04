8298e618927f9fc3292f532411681f78c0e45c22
Ilya <ShigeraIlia@gmail.com>
2023-05-03 20:28:30 -0400

    BuildingBlocks: remove Identifier prop.

--- src/BuildingBlocks/PollutionPatrol.BuildingBlocks.Application/Exceptions/SpecNotFoundException.cs
+++ src/BuildingBlocks/PollutionPatrol.BuildingBlocks.Application/Exceptions/SpecNotFoundException.cs
@@ -2,14 +2,13 @@ namespace PollutionPatrol.BuildingBlocks.Application.Exceptions;
 
 public class SpecNotFoundException : Exception
 {
-    public SpecNotFoundException(string specName, string identifier, string? message = default) : base(message)
+    public SpecNotFoundException(string? specName = default, string? message = default)
+        : base(message)     
     {
         SpecName = specName;
-        Identifier = identifier;
     }
 
-    public string SpecName { get; }
-    public string Identifier { get; }
+    public string? SpecName { get; }
 
-    public override string ToString() => $"Spec \'{SpecName}\' with identifier \'{Identifier}\' is not found: {Message}.";
+    public override string ToString() => $"Spec \'{SpecName}\' is not found: {Message}.";
 }
